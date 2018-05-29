c69bbee2b756b45e588ac1f9974837c6ebd609b4
Giancarlos Salas <giansalex@gmail.com>
2018-05-29 14:53:31 -0500

    first merge option

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 /* eslint-disable semi */
-const pull = require('./src/pull_request');
+const handlePull = require('./src/pull_request');
 
 module.exports = (robot) => {
-  // Your code here
+  const app = robot.route('/home');
+
   robot.log('Yay, the app was loaded!');
-  robot.on('pull_request.opened', pull);
-  robot.on('pull_request.edited', pull);
+  robot.on('pull_request.opened', handlePull);
+
+  app.get('/check', (req, res) => {
+    res.end('Hi, active!')
+  });
 };
--- src/pull_request.js
+++ src/pull_request.js
@@ -1,10 +1,59 @@
+/* eslint-disable semi */
 async function pullRequest (context) {
-    // 1. Extract necessary info
-  const issue = context.issue()
-  const { payload, github, log } = context
-  const { sha } = payload.pull_request.head
+  const { log } = context;
+  const config = await context.config('branch.yml');
 
+  if (!config) {
+    log.warn('Branch Config cannot load');
+    return;
+  }
+  tryMerge();
+}
 
+function tryMerge (config, context) {
+  const { github, payload, log } = context;
+  if (config.autoMerge) {
+    log.warn('AutoMerge: section not found');
+    return;
   }
 
+  if (!canMerge(payload, config.autoMerge)) {
+    return;
+  }
+
+  github.pullRequests.merge(context.issue());
 }
+
+function canMerge (payload, autoMerge) {
+  const isMerged = payload.action === 'closed' && payload.pull_request.merged;
+
+  if (isMerged) {
+    return;
+  }
+
+  const targetBranch = payload.base.ref;
+  const sourceBranch = payload.head.ref;
+
+  const validSource = getSourceBranch(targetBranch, autoMerge);
+
+  if (!validSource) {
+    return;
+  }
+
+  return validSource === sourceBranch;
+}
+
+function getSourceBranch (target, autoMerge) {
+  const len = autoMerge.length;
+  for (let i = 0; i++ < len; i++) {
+    const item = autoMerge[i];
+
+    if (item.target === target) {
+      return item.source;
+    }
+  }
+
+  return null;
+}
+
+module.exports = pullRequest;
