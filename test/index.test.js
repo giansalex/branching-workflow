b560e9f317b037752c6b8483ea1abacf1da61dc2
Giancarlos Salas <giansalex@gmail.com>
2018-05-30 11:16:04 -0500

    add in list test

--- test/index.test.js
+++ test/index.test.js
@@ -7,8 +7,10 @@ const fs = require('fs');
 describe('Branching Workflow', () => {
   let robot;
   let github;
+  let event;
 
   beforeEach(() => {
+    event = JSON.parse(JSON.stringify(payload));
     // Here we create a robot instance
     robot = createRobot();
     // Here we initialize the app on the robot instance
@@ -19,7 +21,6 @@ describe('Branching Workflow', () => {
         merge: jest.fn()
       },
       repos: {
-        createStatus: jest.fn(),
         getContent: jest.fn().mockReturnValue(Promise.resolve({
           data: {
             content: fs.readFileSync('./test/config/automerge.yml').toString('base64')
@@ -32,6 +33,15 @@ describe('Branching Workflow', () => {
   });
 
   describe('Auto Merge', () => {
+    it('Not configured', async () => {
+      event.payload.pull_request.head.ref = 'epd';
+      event.payload.pull_request.base.ref = 'master';
+
+      await robot.receive(event);
+
+      expect(github.pullRequests.merge).not.toHaveBeenCalled();
+    });
+
     it('Match Branch', async () => {
       await robot.receive(payload);
 
@@ -42,11 +52,31 @@ describe('Branching Workflow', () => {
       });
     });
 
-    it('No Match Branch', async () => {
-      let cloned = Object.assign({}, payload);
-      cloned.payload.pull_request.head.ref = 'QAS';
+    it('Match Branch in list', async () => {
+      event.payload.pull_request.head.ref = 'ppr';
+      event.payload.pull_request.base.ref = 'qas';
+      await robot.receive(payload);
 
-      await robot.receive(cloned);
+      expect(github.pullRequests.merge).toHaveBeenCalledWith({
+        owner: 'giansalex',
+        repo: 'portal',
+        number: 1
+      });
+    });
+
+    it('No Match Branch', async () => {
+      event.payload.pull_request.head.ref = 'QAS';
+
+      await robot.receive(event);
+
+      expect(github.pullRequests.merge).not.toHaveBeenCalled();
+    });
+
+    it('No Match Branch in list', async () => {
+      event.payload.pull_request.head.ref = 'ppr';
+      event.payload.pull_request.base.ref = 'epd';
+
+      await robot.receive(event);
 
       expect(github.pullRequests.merge).not.toHaveBeenCalled();
     });
